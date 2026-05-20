f1149aeeb8d92594f89d74607b315775206d8795
Ncristofe <cristofegit@gmail.com>
2026-05-20 09:52:33 -0300

    commit

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-5910460b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.n1u3jue0dqc"
+    "revision": "0.o2cto5cbkqo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
