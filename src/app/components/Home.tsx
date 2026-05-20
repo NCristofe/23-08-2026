b973090ff5d9e430ddeae831719e096401c1fa55
Ncristofe <cristofegit@gmail.com>
2026-05-20 09:49:42 -0300

    fix

--- src/app/components/Home.tsx
+++ src/app/components/Home.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { motion } from "motion/react";
+import { motion } from "framer-motion";
 import { Heart, Calendar } from "lucide-react";
 
 type TimeElapsed = {
