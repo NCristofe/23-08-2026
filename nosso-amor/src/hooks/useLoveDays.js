4e9a74007e839728bf6edbb98138a04a3d07b361
natanael731 <cristofegit@gmail.com>
2026-05-22 10:12:20 -0300

    commit

--- nosso-amor/src/hooks/useLoveDays.js
+++
@@ -1,8 +0,0 @@
-export const useLoveDays = (startDate) => {
-  const today = new Date();
-  const start = new Date(startDate);
-
-  const diff = Math.floor((today - start) / (1000 * 60 * 60 * 24));
-
-  return diff;
-};
