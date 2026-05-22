d16bf48374e5a3f1106e8df305e328c39965b234
natanael731 <cristofegit@gmail.com>
2026-05-22 08:29:50 -0300

    commit

---
+++ nosso-amor/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import { LoveCounter } from "../components/LoveCounter";
+
+export const Home = () => {
+  return (
+    <div>
+      <h1>Nosso Amor ❤️</h1>
+      <LoveCounter />
+    </div>
+  );
+};
