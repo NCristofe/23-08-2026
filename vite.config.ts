dc1bf3c18f31a4e7d7bcb238801bd29e6ddc2925
natanael731 <cristofegit@gmail.com>
2026-05-22 10:30:00 -0300

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -75,5 +75,10 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  server: {
+    watch: {
+      usePolling: true,
+    },
+  },
   assetsInclude: ['**/*.svg', '**/*.csv'],
 })
