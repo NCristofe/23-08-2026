e3fd91ce6706a107360dd623add46f31dc91d946
natanael731 <cristofegit@gmail.com>
2026-05-26 09:06:15 -0300

    update sw.js

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-5910460b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.vrfgipnko8g"
+    "revision": "0.ha59e8ccvjo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
