124cb786d357d1f658eda4274fff44ee612be550
NCristofe <cristofegit@gmail.com>
2026-05-28 11:06:45 -0300

    update sw.js

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-5910460b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.a69fdc9c2h"
+    "revision": "0.kpvsk6fgtc4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
